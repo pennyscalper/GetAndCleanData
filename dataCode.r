6c5d24e21a365b47ecb6bb3baffb2c06441311ae
praveen mac <2praveen.sharma@gmail.com>
2014-08-20 00:46:17 -0400

    full code for GetAndCleanData

--- dataCode.r
+++ dataCode.r
@@ -8,16 +8,26 @@ library(xlsx)
 library(XML)
 library(jsonlite)
 library(data.table)
+onMac = F;
 
-
+if(Sys.getenv("HOME") == "/Users/praveen") {
+  dataLoc = "/Users/praveen/Desktop/DataScience/GetAndCleanData/data";
+  codeLoc = "/Users/praveen/Desktop/DataScience/GetAndCleanData";  
+  onMac = T;
+} else {
 dataLoc = "C:/Documents/modelling/GetAndCleanData/data/";
 codeLoc = "C:/Documents/modelling/GetAndCleanData/";
+}
 setwd(codeLoc)
 
 ###download.file function
 fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
+if(!onMac) {
 setInternet2(TRUE)
 download.file(fileUrl, destfile = "./data/cameras.csv");
+} else {
+  download.file(fileUrl, destfile = "./data/cameras.csv", method = "curl");
+}
 list.files(dataLoc)
 dateDownloaded <- date()
 cameraData <- read.table("./data/cameras.csv", sep = ",", header = TRUE)
@@ -104,4 +114,189 @@ system.time(fread(file))
 
 system.time(read.table(file, header=TRUE, sep="\t"))
 
-###plyr package
+
+###HDF5 package
+source("http://bioconductor.org/biocLite.R")
+biocLite("rhdf5")
+library(rhdf5)
+###creating an hdf5 file names "example.h5"
+created = h5createFile("example.h5")
+created
+
+##creating hierarchical groups 
+created = h5createGroup("example.h5","foo")
+created = h5createGroup("example.h5","baa")
+created = h5createGroup("example.h5","foo/foobaa")
+h5ls("example.h5")
+
+###filling data for the groups and also creating new groups with data
+A = matrix(1:10,nr=5,nc=2)
+h5write(A, "example.h5","foo/A")
+B = array(seq(0.1,2.0,by=0.1),dim=c(5,2,2))
+attr(B, "scale") <- "liter"
+h5write(B, "example.h5","foo/foobaa/B")
+h5ls("example.h5")
+df = data.frame(1L:5L,seq(0,1,length.out=5),
+                c("ab","cde","fghi","a","s"), stringsAsFactors=FALSE)
+h5write(df, "example.h5","df")
+h5ls("example.h5")
+
+###reading data from hdf5 file
+readA = h5read("example.h5","foo/A")
+readB = h5read("example.h5","foo/foobaa/B")
+readdf= h5read("example.h5","df")
+readA
+
+###writing and reading chunks
+h5write(c(12,13,14),"example.h5","foo/A",index=list(1:3,1))
+h5read("example.h5","foo/A")
+
+
+###########Reading data from web....webscrapping
+con = url("http://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en")
+htmlCode = readLines(con)
+close(con)
+htmlCode
+url <- "http://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en"
+html <- htmlTreeParse(url, useInternalNodes=T) ##get the internal nodes 
+xpathSApply(html, "//title", xmlValue) ##no of times the papers are cited by
+
+###reading data from online APIs...examlple twitter
+library(httr)
+##read https://dev.twitter.com/docs/api/1.1/overview and then code
+
+###plyr package...basic idea is split-apply-combine
+library(plyr)
+set.seed(13435)
+X <- data.frame("var1"=sample(1:5),"var2"=sample(6:10),"var3"=sample(11:15))
+X <- X[sample(1:5),]; X$var2[c(1,3)] = NA
+arrange(X, var1); arrange(X, desc(var1)); ##ordering X by column var1
+
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://data.baltimorecity.gov/api/views/k5ry-ef3g/rows.csv?accessType=DOWNLOAD"
+download.file(fileUrl,destfile="./data/restaurants.csv",method="curl")
+restData <- read.csv("./data/restaurants.csv")
+summary(restData); str(restData); 
+table(restData$zipCode,useNA="ifany"); ##use useNA display # of nas
+table(restData$zipCode %in% c("21212","21213"))
+
+data(UCBAdmissions)
+DF = as.data.frame(UCBAdmissions)
+summary(DF)
+##cross table...similar to contingency table if doing counts
+xt <- xtabs(Freq ~ Gender + Admit,data=DF); ##cross tabs
+xt
+##cross tab on more than two variable
+warpbreaks$replicate <- rep(1:9, len = 54)
+xt = xtabs(breaks ~.,data=warpbreaks)
+xt
+##converting above cross tab into flat table
+ftable(xt)
+
+###size of the objects
+fakeData = rnorm(1e5)
+object.size(fakeData)
+print(object.size(fakeData),units="Mb") ##in Mb
+
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://data.baltimorecity.gov/api/views/k5ry-ef3g/rows.csv?accessType=DOWNLOAD"
+download.file(fileUrl,destfile="./data/restaurants.csv",method="curl")
+restData <- read.csv("./data/restaurants.csv")
+
+##easier cutting of the data
+library(Hmisc)
+restData$zipGroups = cut2(restData$zipCode,g=4)
+table(restData$zipGroups)
+###doing the same using mutate function from plyr package
+restData2 = mutate(restData,zipGroups=cut2(zipCode,g=4))
+table(restData2$zipGroups)
+###melting the data frame to reshape
+library(reshape2)
+head(mtcars)
+mtcars$carname <- rownames(mtcars)
+carMelt <- melt(mtcars,id=c("carname","gear","cyl"),measure.vars=c("mpg","hp"))
+##melt creates 3 id columns and one variable column with name mpg or hp with its value in the column value
+head(carMelt,n=3)
+###decasting the melted data frame
+cylData <- dcast(carMelt, cyl ~ variable, fun.aggregate = mean) ##just like aggregate fuction
+cylData
+###using split apply and combine
+head(InsectSprays)
+spIns =  split(InsectSprays$count,InsectSprays$spray) ###split
+spIns
+##lapply...apply a function to individual list
+sprCount = lapply(spIns,sum) ###apply
+sprCount
+unlist(sprCount); ##combine...to get it in vector form
+sapply(spIns,sum); ##apply and combine in in one step
+
+###another way using ddply from plyr package which splits apply and combine in one commanc
+ddply(InsectSprays,.(spray),summarize,sum=sum(count))
+
+###merging more than one data frame using join_all in package plyr
+df1 = data.frame(id1=sample(1:10),x=rnorm(10))
+df2 = data.frame(id1=sample(1:10),y=rnorm(10))
+df3 = data.frame(id1=sample(1:10),z=rnorm(10))
+dfList = list(df1,df2,df3)
+arrange(join_all(dfList), desc(id1)); arrange(join_all(dfList), id1); #second one in ascending order of id1
+
+####Editing text variables...regular expressions etc
+tolower(LETTERS[1:10]); #toupper
+
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
+download.file(fileUrl,destfile="./data/cameras.csv",method="curl")
+cameraData <- read.csv("./data/cameras.csv")
+names(cameraData)
+
+###truncating the names after "."
+splitNames = strsplit(names(cameraData),"\\.")
+firstElement <- function(x){x[1]}
+sapply(splitNames,firstElement)
+
+fileUrl1 <- "https://dl.dropboxusercontent.com/u/7710864/data/reviews-apr29.csv"
+fileUrl2 <- "https://dl.dropboxusercontent.com/u/7710864/data/solutions-apr29.csv"
+download.file(fileUrl1,destfile="./data/reviews.csv",method="curl")
+download.file(fileUrl2,destfile="./data/solutions.csv",method="curl")
+reviews <- read.csv("./data/reviews.csv"); solutions <- read.csv("./data/solutions.csv")
+head(reviews,2)
+
+grep("Alameda",cameraData$intersection)
+table(grepl("Alameda",cameraData$intersection)) ##grep logical...returns vector of T and F
+cameraData2 <- cameraData[!grepl("Alameda",cameraData$intersection),]
+grep("Alameda",cameraData$intersection,value=TRUE) ##gives out the value
+
+library(stringr)
+nchar("Jeffrey Leek")
+paste0("Jeffrey","Leek") ##paste with no spcae in between
+str_trim("Jeff      ") ##trims the white spaces at the start and end of the string
+
+###regular expression
+##literals are where we need exact match
+##^i think will match any line starting with "I think"
+##morning$ will match any line ending with morning
+##[] is called character class
+##[Bb][Uu][Ss][Hh] will match any upper and lower case combination in word "bush"
+##^[Ii] am  will match ^I am or ^i am at the start of the line
+##^[0-9][a-zA-Z] will match anything like "[numeric][Letter]) at the start of the line
+##[^?.]$...$ means matching end of the line..^ inside [] means not matching anything after ^
+##hence it will match anything where last character of the line is not ? or .
+##special character "."...means any one character..hence 9.11 will match anything 9-11 or 9a11 or 9111 etc
+##metacharacte "|" means OR...so fire|flood will match any line containing fire or flood in it
+##^[Gg]ood|[Bb]ad meaning "good or Good at the start of the line" or "Bad or bad anywhere in the line"
+##^([Gg]ood|[Bb]ad) good or Good or Bad or bad at the start of the line"
+##[Gg]eorge( [Ww]\.)? [Bb]ush...? after paranthesis is optional part..so it will match 
+##george or George followed by W or blank or . and then Bush or bush.
+##adding backslash here means dont consider "." to be special character
+##"*" meaning search anything anynumber or times..."+" atleast once
+##(.*) matching a line with paranthesis with anything inside it
+##[0-9]+ (.*)[0-9]+ matches anynumber atleast once followed by anything 
+##then followed by any number atleast once
+##{ and } are referred to as interval quantifiers; the let us specify the minimum 
+##and maximum number of matches of an expression
+##[Bb]ush( +[^ ]+ +){1,5} debate
+
+
+
+
+
