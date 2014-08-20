432889e4dd056648ffe1cd6ad45ddc30ba7b381c
praveen windows <2praveen.sharma@gmail.com>
2014-08-20 17:57:02 -0400

    more changes

--- dataCode.r
+++ dataCode.r
@@ -75,7 +75,7 @@ jsonData$owner$login
 myjson = toJSON(iris, pretty = T);
 iris = fromJSON(myjson);
 
-####data.table!! 
+##############################################################################################################################data.table!! 
 ####its inherited from data.frame. Much faster than data.frame
 DT = data.table(x=rnorm(9),y=rep(c("a","b","c"),each=3),z=rnorm(9))
 head(DT,3)
@@ -104,7 +104,66 @@ DT['a'] ##very fast
 DT1 <- data.table(x=c('a', 'a', 'b', 'dt1'), y=1:4)
 DT2 <- data.table(x=c('a', 'b', 'dt2'), z=5:7)
 setkey(DT1, x); setkey(DT2, x)
+DT1["a"]; DT1["a",]; ##putputs column x == "a" rows
+##for multiple cols keys DT1[J("a", "b", "c")]  ==> if key has three cols then this will output rows where col1 == "a" & col2 == "b" & col3 == "c"
 merge(DT1, DT2)
+
+###J(join), CJ and SJ
+DT = CJ(x = letters[1:5], y = runif(3), z = c("aa", "bb")); ##A data.table is formed from the cross product of the vectors. For example, 10 ids, and 100 dates, CJ returns a 1000 row table containing all the dates for all the ids.
+DT[, x1:= sample(LETTERS, nrow(DT), replace = T)];
+DT[, .N, by=list(x, x1)]; DT[, mean(y), by=list(x, x1)]
+setkey(DT, x, x1);
+DT[J("d", "T")]; DT[.("b", "H")]; DT[list("b", "H")]; ##all outputs same and is very fast because keys are sorted and this runs the binary search 
+DT[x == "d" &  x1 == "T"]; ##using data.table badly and is as slow as data frame
+##J("d", "T") is a data.table when written inside DT[]
+
+###showing the speed differences between data.frame and data.table
+grpsize = ceiling(1e7/26^2); tt=system.time( DF <- data.frame(x=rep(LETTERS,each=26*grpsize),y=rep(letters,each=grpsize),v=runif(grpsize*26^2),stringsAsFactors=FALSE))
+system.time(ans1 <- DF[DF$x=="R" & DF$y=="h",])
+DT = as.data.table(DF); system.time(setkey(DT,x,y)); system.time(ans2 <- DT[J("R","h")])
+identical( DT[J("R","h"),], DT[data.table("R","h"),]) ##returns true 
+DT[CJ(c("A","R"),c("h", "a")),]; #CJ creates a DF with crossing c("A","R") and c("h", "a"), hence 4 rows. joins that with DT to subset DT. this is similar to merge but much faster
+DT[CJ(c("A","R"),c("h", "a")), list(count = .N, avg = mean(v), sd = sd(v)), by="x,y"] #does what table function does
+#########################3. Fast time series join
+#This is also known as last observation carried forward (LOCF) or a rolling join.
+#Recall that x[i] is a join between data.table x and data.table i. If i has 2 columns, the
+#First column is matched to theFirst column of the key of x, and the 2nd column to the 2nd. An
+#equi-join is performed, meaning that the values must be equal.
+#The syntax for fast rolling join is
+#x[i,roll=TRUE]
+#As before the First column of i is matched to x where the values are equal. The last column
+#of i though, the 2nd one in this example, is treated specially. If no match is found, then the row
+#before is returned, provided theFirst column still matches.
+#For examples type 
+DT = data.table(x=rep(c("a","b","c"),each=3), y=c(1,3,6), v=1:9); setkey(DT,x)
+example(data.table)
+DT[,.SD[which.min(v)],by=x][] # nested query by group; .SD is subset data broken by "by". then on individual SD applying which.min(v)
+DT[,.SD[2],by=x] # 2nd row of each group
+DT[,tail(.SD,2),by=x] # last 2 rows of each group
+DT[,lapply(.SD,sum),by=x] # apply through columns by group
+DT[,lapply(tail(.SD,2), sum),by=x] # last 2 rows of each group and on top of that do sum of each column in every subset by applying lapply
+setkey(DT,x,y)
+DT[,list(MySum=sum(v), MyMin=min(v), MyMax=max(v)), by=list(x,y%%2)] # by 2 expressions
+DT[,sum(v),x][V1<20]
+print(DT[,z:=42L]) # add new column by reference
+print(DT[,z:=NULL]) # remove column by reference
+print(DT["a",v:=42L]) # subassign to existing v column by reference
+DT[!J("a")] # not join
+DT[!"a"] # same
+DT[!2:4] # all rows other than 2:4
+DT[x!="b" | y!=3] # multiple vector scanning approach, slow
+DT[!J("b",3)] # same result but much faster
+
+DT = data.table(a=LETTERS[c(1,1:3)],b=4:7,key="a")
+DT[2,d:=10L] # subassign by reference to column d; where key == 2 it puts d = 10 and rest NA
+duplicated(DT); #runs it on keys by default if variable by is not provided
+unique(DT); #runs it on keys by default if variable by is not provided 
+
+###Fast Grouping
+DT[,sum(v),by=x]
+###Compare
+ttt=system.time(tt <- tapply(DT$v,DT$x,sum)); ttt
+sss=system.time(ss <- DT[,sum(v),by=x]); sss
 
 ###Fast reading
 big_df <- data.frame(x=rnorm(1E6), y=rnorm(1E6))
@@ -113,7 +172,7 @@ write.table(big_df, file=file, row.names=FALSE, col.names=TRUE, sep="\t", quote=
 system.time(fread(file))
 
 system.time(read.table(file, header=TRUE, sep="\t"))
-
+##############################################################################################################################Finished data.table!! 
 
 ###HDF5 package
 source("http://bioconductor.org/biocLite.R")
@@ -215,7 +274,7 @@ library(reshape2)
 head(mtcars)
 mtcars$carname <- rownames(mtcars)
 carMelt <- melt(mtcars,id=c("carname","gear","cyl"),measure.vars=c("mpg","hp"))
-##melt creates 3 id columns and one variable column with name mpg or hp with its value in the column value
+##melt creates 2 columns and one "variable" column with value "mpg" or "hp" and one "value" column
 head(carMelt,n=3)
 ###decasting the melted data frame
 cylData <- dcast(carMelt, cyl ~ variable, fun.aggregate = mean) ##just like aggregate fuction
