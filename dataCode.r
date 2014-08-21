9daeb90a01ba0e3792f65057a571715e1099bc33
praveen windows <2praveen.sharma@gmail.com>
2014-08-21 18:11:00 -0400

    another change

--- dataCode.r
+++ dataCode.r
@@ -159,6 +159,26 @@ DT[2,d:=10L] # subassign by reference to column d; where key == 2 it puts d = 10
 duplicated(DT); #runs it on keys by default if variable by is not provided
 unique(DT); #runs it on keys by default if variable by is not provided 
 
+###some transformation done by transform or within can be done by := in data table
+DT <- data.table(a=rep(1:3, each=2), b=1:6)
+DT2 <- transform(DT, c = a^2)
+DT[, c:=a^2]
+identical(DT,DT2)
+DT2 <- within(DT, {b <- rev(b) 
+c <- a*2 
+rm(a)})
+DT[,':='(b = rev(b), c = a*2, a = NULL)]
+identical(DT,DT2)
+
+DT$d = ave(DT$b, DT$c, FUN=max) # copies entire DT, even if it is 10GB in RAM
+DT = DT[, transform(.SD, d=max(b)), by="c"] # same, but even worse as .SD is copied for each group
+DT[, d:=max(b), by="c"] # same result, but much faster, shorter and scales
+# Multiple update by group. Convenient, fast, scales and easy to read.
+DT[, `:=`(minb = min(b),
+meanb = mean(b),
+bplusd = sum(b+d)), by=c%/%5]
+
+
 ###Fast Grouping
 DT[,sum(v),by=x]
 ###Compare
@@ -251,6 +271,7 @@ xt = xtabs(breaks ~.,data=warpbreaks)
 xt
 ##converting above cross tab into flat table
 ftable(xt)
+
 
 ###size of the objects
 fakeData = rnorm(1e5)
